069cf96e8110b170ccef797e4fb84c7822ef81b4
Deepak <dk774369@gmail.com>
2025-03-20 21:44:02 +0530

    Added login route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -10,7 +10,7 @@ module.exports.registerUser = async (req, res, next) => {
         return res.status(400).json({errors: errors.array() })
     }
 
-    
+
 
     const { fullname, email, password } = req.body
 
@@ -26,4 +26,29 @@ module.exports.registerUser = async (req, res, next) => {
     const token = user.generateAuthToken()
 
     res.status(201).json({ token, user})
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array() })  
+    }
+
+    const { email, password } = req.body
+
+    const user = await userModel.findOne({ email}).select('+password')
+
+    if(!user){
+        return res.status(401).json({ message: 'Invalid email or password'})
+    }
+
+    const isMatch = await user.comparePassword(password)
+
+    if(!isMatch){
+        return res.status(401).json({ message: 'Invalid email or password' })
+    }
+
+    const token = user.generateAuthToken()
+
+    res.status(200).json({ token, user})
 }
--- Backend/readme.md
+++ Backend/readme.md
@@ -30,3 +30,30 @@ Used to register a new user. Requires firstname, lastname, email, and password.
     -`email` (string): user's email address (must be a valis format).
     -`password` (string): User's password (minimum 6 characters).
 -`token` (String): JWT Token
+
+## /users/login
+
+### Description
+Used to authenticate an existing user.
+
+## Request
+**Method:** POST  
+**URL:** `/users/login`
+
+**Body format:**
+```json
+{
+  "email": "johndoe@example.com",
+  "password": "password123"
+}
+```
+### Example Response
+
+- `user` (object):
+    -`fullname` (object).
+        -`firstname` (string): User's first name (minimum 3 characters).
+        -`lastname` (string): User's last name (minimum 3 characters).
+    -`email` (string): user's email address (must be a valis format).
+    -`password` (string): User's password (minimum 6 characters).
+-`token` (String): JWT Token
+
